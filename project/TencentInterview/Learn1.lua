9359b21ac587ee3355d15ab2ddbae4bde63badef
talentduanbin@163.com <htuDbb4073>
2019-06-03 16:24:49 +0800

    添加派生类

--- project/TencentInterview/Learn1.lua
+++ project/TencentInterview/Learn1.lua
@@ -13,7 +13,7 @@ end
 
 -- 基础类方法 printArea
 function A:foo ()
-  print("fromA "..self.name..self.age)
+  print("from A"..self.name..self.age)
 end
 
 -- 创建对象
@@ -21,3 +21,20 @@ local a = A:new()
 a.name = "hanmeimei"
 a.age = 17
 a:foo()
+
+B = A:new()
+function B:new( o )
+  o = o or A:new(o)
+  setmetatable(o,self)
+  self.__index = self
+  return o
+end
+
+function B:foo(  )
+  print("from B"..self.name..self.age)
+end
+
+local b = B:new()
+b.name = "lilei"
+b.age = 18
+b:foo()
