bbd2eb80865b4d6e3e1594b26427fea35e31c943
DuanBinbin <talentduanbin@163.com>
2019-06-12 11:56:45 +0800

    添加String类型的操作

--- project/TencentInterview/Question2.lua
+++ project/TencentInterview/Question2.lua
@@ -1,7 +1,13 @@
+--lua中的字符串不可变的
+--lua中能够操作100K或者1M的字符串
 local inputParams = "80 + 22 * (33 - 44) / 55 * (15 + 78)"
 
-srcstr = tonumber(inputParams) 
+local numberArray = {0,1,2,3,4,5,6,7,8,9}
+local operatorArray = {"+","-","*","/","%","^","-"} --lua中如何区分减号和负号
+local priority = {"(",")"}
 
+--判断是否可以转化为数字
+srcstr = tonumber(inputParams) 
 if srcstr then
     print("可以转化为number")        
 else
@@ -15,5 +21,3 @@ function calculate( params )
     local retValue = 80 + 22 * (33 - 44) / 55 * (15 + 78)    
     return retValue;
 end
-
-print(calculate(inputParams)) 
